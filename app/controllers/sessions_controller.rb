c99b072bbd2a09f86623345071bcfadde4d82017
KamilahJ4e <kamilahjae@gmail.com>
2014-11-18 16:05:31 -0800

    create a user in session controller and redirect to home page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,27 @@ class SessionsController < ApplicationController
 
     if @provider.nil?
       @provider = Provider.create_with_omniauth(auth)
+      @user = User.create
+      @provider.user_id = @user.id
+      @provider.save
+
     end
 
     if signed_in?
       if @provider.user == current_user
-        redirect_to root_path, notice: "Already linked that account!"
+        # redirect_to "/", notice: "Already linked that account!"
       else
         @provider.user = current_user
         @provider.save()
-        redirect_to root_path, notice: "Successfully linked that account!"
+        # redirect_to "/", notice: "Successfully linked that account!"
       end
     else
       if @provider.user.present?
         self.current_user = @provider.user
-        redirect_to root_path, notice: "Please finish registering"
+        # redirect_to "/", notice: "Please finish registering"
       end
     end
+    redirect_to "/"
   end
 
   def destroy
