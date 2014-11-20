171df3790c59a5be12a016dc7fe8e472a4a33453
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-20 15:38:38 -0800

    Added validations back in

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,7 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
 
-  
+  validates :media, presence: true
+  validates :local_id, presence: true
+  validates :user_id, presence: true
 end
