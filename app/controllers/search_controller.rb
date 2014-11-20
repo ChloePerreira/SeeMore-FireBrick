91c8d74eee8799bdf6eea213627cc26f1fdb3204
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-19 17:16:28 -0800

    Clean up code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,23 +2,13 @@ class SearchController < ApplicationController
 
   def index
     @response = search_twitter_user
-    #@response = results
-    #raise
   end
 
   def search_twitter_user
-    # with search form, once screen_name is submitted, data will return in separate
-    # search results view page.
-    # regex? fuzzy search?
     set_twitter_client
     query = params[:twitter_search]
     set_twitter_client.user_search(query)
-    # raise
-    # #response = HTTParty.get(" https://api.twitter.com/1.1/users/show.json?screen_name=#{query}"
-    # redirect_to twitter_results_path
-    #raise.params
   end
-
 
   private
 
