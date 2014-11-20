284e4ba0db36af9646dd91bb314cf76a678a3b38
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-20 10:40:21 -0800

    Added previous page and results limit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,18 +2,22 @@ class SearchController < ApplicationController
 
   def index
     @response = search_twitter_user
-    @current_page = params[:twitter_page].to_i || 1
     @query = params[:twitter_search]
-    if @current_page == 0 
-      @current_page = 1
-    end
+    @current_page = @twitter_page
+    @previous_page = @current_page - 1
+    @next_page = @current_page + 1
   end
 
   def search_twitter_user
     set_twitter_client
-    twitter_page = params[:twitter_page].to_i
+    @twitter_page = params[:twitter_page].to_i
+
+    if @twitter_page == 0
+      @twitter_page = 1
+    end
+
     @query = params[:twitter_search]
-    set_twitter_client.user_search(@query, {page: twitter_page})
+    set_twitter_client.user_search(@query, {page: @twitter_page})
   end
 
   private
