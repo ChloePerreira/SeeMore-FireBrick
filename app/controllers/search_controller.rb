c13b77a00750caf745337e9238840e4a89140cb2
Kathy Lepe <kathylepe434@gmail.com>
2014-11-21 14:50:47 -0800

    add geemo gem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
       @response = search_twitter_user
       results
       render "_twitter_results"
-    else
+    elsif params[:provider] == "vimeo"
       @response = search_vimeo_user
       results
       render "_vimeo_results"
@@ -14,9 +14,9 @@ class SearchController < ApplicationController
 
   def results
     @query = params[:provider_search]
-    @current_page = @provider_page
-    @previous_page = @current_page - 1
-    @next_page = @current_page + 1
+    # @current_page = @provider_page
+    # @previous_page = @current_page - 1
+    # @next_page = @current_page + 1
 
   end
 
@@ -30,5 +30,9 @@ class SearchController < ApplicationController
 
     @query = params[:provider_search]
     set_twitter_client.user_search(@query, {page: @provider_page})
+  end
+
+  def search_vimeo_user
+    @query = Beemo::User.search(params[:provider_search])
   end
 end
