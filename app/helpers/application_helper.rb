85a6e38696dcb6daedf8832b7d58add793ae11e1
KamilahJ4e <kamilahjae@gmail.com>
2014-11-25 16:01:55 -0800

    corrected is_are helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def is_are(count)
     count > 1 ? "are the top users" : "is the top user"
   end
+
+  def replace_url#insert method to insert anchor tag there, handle multiple urls
+  end
 end
